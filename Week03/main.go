395c1dfa0cb3f760a401a5782bff171bd8b5ca70
lsw45 <2221717948@qq.com>
2020-12-09 23:26:27 +0800

    log

--- Week03/main.go
+++ Week03/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/golang/sync/errgroup"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	var (
-		err error
+		err  error
+		stop = make(chan os.Signal, 1)
 	)
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
 	})
+	server := http.Server{Addr: ":8080", Handler: mux}
 
 	g := errgroup.Group{}
 
 	g.Go(func() (err error) {
-		if err = http.ListenAndServe(":8080", nil); err != nil {
+		if err = server.ListenAndServe(); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	signal.Notify(stop, syscall.SIGINT|syscall.SIGTERM|syscall.SIGKILL)
+	g.Go(func() error {
+		c := <-stop
+		fmt.Printf("Got signal:%v\n", c)
+		if err = server.Shutdown(context.TODO()); err != nil {
 			return err
 		}
 		return nil
